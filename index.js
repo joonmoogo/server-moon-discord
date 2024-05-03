65342704b0f3cecfcce016a3b90c6037c2aa0384
joonmoogo <junemuk10@gmail.com>
2024-05-03 14:27:12 +0900

    user logic

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const {Server} = require('socket.io')
+const { Server } = require('socket.io')
 const http = require('http')
 const cors = require('cors')
 app.use(cors());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<id>:<password>@cluster0.deeugr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://junemuk:<password>@cluster0.deeugr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -30,16 +30,30 @@ async function run() {
 }
 run().catch(console.dir);
 
-async function insertDocument(document) {
+async function insertDocument(collectionName, document) {
   const client = new MongoClient(uri);
 
   try {
     const database = client.db('moon-discord');
-    const collection = database.collection('moon-discord'); // 사용할 컬렉션 이름
+    const collection = database.collection(collectionName); // 사용할 컬렉션 이름
 
     // 데이터 생성
     const result = await collection.insertOne(document);
     console.log(`Inserted document with _id: ${result.insertedId}`);
+  } finally {
+    client.close();
+  }
+}
+
+async function getDocuments(collectionName, query) {
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db('moon-discord');
+    const collection = database.collection(collectionName); // 사용할 컬렉션 이름
+
+    const result = await collection.findOne(query);
+    return result;
   } finally {
     client.close();
   }
@@ -57,20 +71,41 @@ const io = new Server(server, {
   },
 });
 
-app.get('/',(req,res)=>{
-    res.send('<h1>hello express</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>hello express</h1>')
 })
 
 
 
-
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on('message',(data)=>{
-      console.log(data);
-      insertDocument(data);
-      socket.broadcast.emit('message',data);
-    })
-  });
+  socket.on('user', async (data) => {
+    const { name } = data;
+    const result = await getDocuments('User', { name: name })
+    if(result == null){
+      const newUser = {
+        name: name,
+        friends: [],
+        ChattingRooms: [],
+      }
+      await insertDocument('User', newUser);
+      socket.emit('User',newUser);
+    }
+    else{
+      socket.emit('User',result);
+    }
+  })
+
+  socket.on('message', (data) => {
+    console.log(data);
+    // insertDocument(data);
+    socket.broadcast.emit('message', data);
+  })
+
+  socket.on('channel', (data) => {
+    const { name } = data;
+
+  })
+});
 
