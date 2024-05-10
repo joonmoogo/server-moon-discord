fec3ce4dc08fdbc962f573606ed6e8a8ca978f29
joonmoogo <junemuk10@gmail.com>
2024-05-10 13:52:19 +0900

    Co-authored-by: L4HO <L4HO@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const {Server} = require('socket.io')
+const { Server } = require('socket.io')
 const http = require('http')
 const cors = require('cors')
 app.use(cors());
@@ -56,19 +56,111 @@ const io = new Server(server, {
   },
 });
 
-app.get('/',(req,res)=>{
-    res.send('<h1>hello express</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>hello express</h1>')
 })
 
 
 
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on('message',(data)=>{
-      console.log(data);
-      insertDocument(data);
-      socket.broadcast.emit('message',data);
-    })
+  socket.on('message', (data) => {
+    console.log(data);
+    insertDocument(data);
+    socket.broadcast.emit('message', data);
+  })
+
+  socket.on('user',(async (username)=>{
+    await findUser(username.name);
+  }));
+
+  const findUser = async (username) => {
+    const client = new MongoClient(uri);
+
+    try {
+      const database = client.db('moon-discord');
+      const collection = database.collection('User'); // 사용할 컬렉션 이름
+      const data = await collection.findOne({username:username})
+      /*유저 있는 경우 */
+      if(data){
+        socket.emit('user',data);
+      }
+      /*유저 없는 경우 */
+      else{
+        const userObject = {
+          username:username,
+          friends:[],
+          channels:[]
+        }
+        await collection.insertOne(userObject)
+        socket.emit('user',userObject);
+      }
+    }
+    finally{
+      client.close();
+    }
+    }
+  /** 
+   @description
+   * 
+  */
+  // 유저 생성
+  // parameter = Username: string
+  // username받아서 mongoDB저장
+  // 만약 username이 mongoDB에 있다면.. 가져와서 socket.emit('user',data)
+  // 없다면 생성하고 socket.emit('user',data)
+  // username:string
+  // userChannel:[] //유저가 속한 채널들 배열
+  // friends:[] //유저 친구들 
+
+  // collection name = 'User'
+
+  // socket.on('channel');
+    // 채널 생성
+    // parameter = ChannelName : string
+    // channelName받아서 mongoDB저장
+
+    // 만약 channelName이 mongoDB에 있다면.. 가져와서 socket.emit('channel',data)
+    // 없다면 생성하고 socket.emit('channel',data)
+
+    // channelName : string
+    // channelUsers:[] // 유저 이름들 배열
+    // chattingLogs:[] // 채팅방 기록들
+    // collection name = 'Channel'
+
+    // socket.emit('channel',방금 추가한 채널)
+
+    // socket.on('channelJoin');
+    // 채널 입장
+    // parameter = {username:string, channelName:string}
+    // await db에서 채널이름 찾아서 channelUsers 배열에 유저 추가
+    // await db에서 유저이름 찾아서 userChannel 배열에 채널 추가
+    // await db에서 채널정보 channelName,chattingUsers,chattingLogs 찾아서 socket.emit('channeljoin',데이터)
+
+
+
+    // socket.on('friend');
+    // 친구 생성
+    // parameter = {username:string, friendName:string}
+    // db에서 username 찾아서 friends배열에 friendName 추가
+    // collection name = 'User'
+
+    // socket.emit('friend',방금 추가한 친구)
+
+    // socket.on('message');
+    // 메세지 전달
+    // parameter = {username:string, userMessage:string, channel:string}
+    // db에서 채널 이름 찾아서 chattingLogs 배열에 메시지와 메세지 생성시간 추가
+    // ex) {username:string, userMessage:string, createdAt:Timedata }
+    // collection name = 'Channel'
+
+    // socket.emit('message',방금 받은 채팅 기록) 
+    // *브로드캐스팅.. 채널에 속한 사람들에게 전달해야함. 
+
+
+    // flow 유저생성 -> 채널생성 -> 채널입장 -> 메세지 전달
+
   });
+
