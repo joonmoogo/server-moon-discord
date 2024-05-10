1d74503bc36e1ef84f63fa0171552b7b66010eca
joonmoogo <junemuk10@gmail.com>
2024-05-10 12:38:53 +0900

    rollback

--- index.js
+++ index.js
@@ -1,121 +1,42 @@
 const express = require("express");
 const app = express();
-const { Server } = require('socket.io')
+const {Server} = require('socket.io')
 const http = require('http')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
 app.use(cors());
 
-const uri = "mongodb+srv://junemuk:1998born@cluster0.deeugr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1,strict: true,deprecationErrors: true,}})
-async function mongoRun() {
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://junemuk:1998born@cluster0.deeugr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch {
-    throw new Error('App can not connect to database');
-  }
-  finally {
+  } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-mongoRun();
+run().catch(console.dir);
 
-const server = http.createServer(app);
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING ON PORT 3001");
-});
+async function insertDocument(document) {
+  const client = new MongoClient(uri);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-app.get('/', (req, res) => {
-  res.send('<h1>hello express</h1>')
-})
-
-
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on('user', async (data) => {
-    const { name } = data;
-    if (name == null) return 0;
-    const result = await getDocuments('User', { name: name })
-    if (result == null) {
-      const newUser = {
-        name: name,
-        friends: [],
-        ChattingRooms: [],
-      }
-      await insertDocument('User', newUser);
-      socket.emit('User', newUser);
-    }
-    else {
-      socket.emit('User', result);
-    }
-  })
-
-  socket.on('friend', async (data) => {
-    const { userId, friendId } = data;
-    if (friendId == null) return 0;
-    const result = await getDocuments('User', { friends: friendId })
-    if (result == null) {
-      const newFriend = await getDocuments('User', { _id: friendId });
-      await updataeDocuments('User',
-        { id: userId },
-        {
-          $push: {
-            friends: newFriend
-          }
-        }
-      )
-      socket.emit('friend', newFriend);
-    }
-  })
-
-  socket.on('message', async (data) => {
-    const { roomId, message } = data;
-    if (message == null) return 0;
-
-    await updataeDocuments('ChattingRoom',
-      { _id: roomId },
-      {
-        $push: {
-          roomMessage: message
-        }
-      }
-    )
-    socket.broadcast.emit('message', data);
-  })
-
-  socket.on('channel', async (data) => {
-    const { name } = data;
-    if (name == null) return 0;
-    const roomInfo = {
-      roomName: name,
-      roomParticipant: [],
-      roomMessage: [],
-    }
-    await insertDocument('ChattingRoom',roomInfo)
-    socket.emit('channel', roomInfo)
-  })
-});
-
-
-async function insertDocument(collectionName, document) {
   try {
     const database = client.db('moon-discord');
-    const collection = database.collection(collectionName); // 사용할 컬렉션 이름
+    const collection = database.collection('moon-discord'); // 사용할 컬렉션 이름
+
+    // 데이터 생성
     const result = await collection.insertOne(document);
     console.log(`Inserted document with _id: ${result.insertedId}`);
   } finally {
@@ -123,30 +44,31 @@ async function insertDocument(collectionName, document) {
   }
 }
 
-async function getDocuments(collectionName, query) {
-  const client = new MongoClient(uri);
+const server = http.createServer(app);
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
 
-  try {
-    const database = client.db('moon-discord');
-    const collection = database.collection(collectionName); // 사용할 컬렉션 이름
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-    const result = await collection.findOne(query);
-    return result;
-  } finally {
-    client.close();
-  }
-}
+app.get('/',(req,res)=>{
+    res.send('<h1>hello express</h1>')
+})
 
-async function updataeDocuments(collectionName, query) {
-  const client = new MongoClient(uri);
 
-  try {
-    const database = client.db('moon-discord');
-    const collection = database.collection(collectionName); // 사용할 컬렉션 이름
 
-    const result = await collection.updateOne(query);
-    return result;
-  } finally {
-    client.close();
-  }
-}
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on('message',(data)=>{
+      console.log(data);
+      insertDocument(data);
+      socket.broadcast.emit('message',data);
+    })
+  });
