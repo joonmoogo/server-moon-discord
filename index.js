1d5839701e937581a647d403450d2c9a21e7a4be
joonmoogo <junemuk10@gmail.com>
2024-05-10 15:37:07 +0900

    Co-authored-by: L4HO <L4HO@users.noreply.github.com>

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ io.on("connection", (socket) => {
     socket.broadcast.emit('message', data);
   })
 
-  socket.on('user',(async (username)=>{
+  socket.on('user', (async (username) => {
     await findUser(username.name);
   }));
   /** 
@@ -82,6 +82,7 @@ io.on("connection", (socket) => {
    username받아서 mongoDB저장
    만약 username이 mongoDB에 있다면.. 가져와서 socket.emit('user',data)
    없다면 생성하고 socket.emit('user',data)
+   
    username:string
    userChannel:[] 유저가 속한 채널들 배열
    friends:[] //유저 친구들 
@@ -89,158 +90,183 @@ io.on("connection", (socket) => {
    collection name = 'User'
  
   */
-  
- 
-  
-   const findUser = async (username) => {
+
+
+
+  const findUser = async (username) => {
     const client = new MongoClient(uri);
     try {
       const database = client.db('moon-discord');
       const collection = database.collection('User'); // 사용할 컬렉션 이름
-      const data = await collection.findOne({username:username})
+      const data = await collection.findOne({ username: username })
       /*유저 있는 경우 */
-      if(data){
-        socket.emit('user',data);
+      if (data) {
+        socket.emit('user', data);
       }
       /*유저 없는 경우 */
-      else{
+      else {
         const userObject = {
-          username:username,
-          friends:[],
-          channels:[]
+          username: username,
+          friends: [],
+          channels: []
         }
         await collection.insertOne(userObject)
-        socket.emit('user',userObject);
+        socket.emit('user', userObject);
       }
     }
-    finally{
+    finally {
       client.close();
     }
-    }
-  
-    
-    // 채널 생성
-    // parameter = ChannelName : string
-    // channelName받아서 mongoDB저장
+  }
 
-    // 만약 channelName이 mongoDB에 있다면.. 가져와서 socket.emit('channel',data)
-    // 없다면 생성하고 socket.emit('channel',data)
 
-    // channelName : string
-    // channelUsers:[] // 유저 이름들 배열
-    // chattingLogs:[] // 채팅방 기록들
-    // collection name = 'Channel'
+  // 채널 생성
+  // parameter = ChannelName : string
+  // channelName받아서 mongoDB저장
 
-    socket.on('channel', (async (channelName)=>{
-      await findChannel(channelName.channelName);
-     }));
+  // 만약 channelName이 mongoDB에 있다면.. 가져와서 socket.emit('channel',data)
+  // 없다면 생성하고 socket.emit('channel',data)
 
-     
-const findChannel = async (channelName) => {
+  // channelName : string
+  // channelUsers:[] // 유저 이름들 배열
+  // chattingLogs:[] // 채팅방 기록들
+  // collection name = 'Channel'
+
+  socket.on('channel', (async (channelName) => {
+    await findChannel(channelName.channelName);
+  }));
+
+
+  const findChannel = async (channelName) => {
     const client = new MongoClient(uri);
     try {
       const database = client.db('moon-discord');
       const collection = database.collection('Channel'); // 사용할 컬렉션 이름
-      const data = await collection.findOne({channel:channelName})
+      const data = await collection.findOne({ channel: channelName })
       /*채널 있는 경우 */
-      if(data){
-        socket.emit('channel',data);
+      if (data) {
+        socket.emit('channel', data);
       }
       /*채널 없는 경우 */
-      else{
+      else {
         const channelObject = {
-          channelName : channelName,
-          channelUsers:[], // 유저 이름들 배열
-          chattingLogs:[], // 채팅방 기록들
+          channelName: channelName,
+          channelUsers: [], // 유저 이름들 배열
+          chattingLogs: [], // 채팅방 기록들
         }
         await collection.insertOne(channelObject)
-        socket.emit('channel',channelObject);
+        socket.emit('channel', channelObject);
       }
     }
-    finally{
+    finally {
       client.close();
     }
+  }
+  // socket.emit('channel',방금 추가한 채널)
+
+  // socket.on('channelJoin');
+  // 채널 입장
+  // parameter = {username:string, channelName:string}
+  // await db에서 채널이름 찾아서 channelUsers 배열에 유저 추가
+  // await db에서 유저이름 찾아서 userChannel 배열에 채널 추가
+  // await db에서 채널정보 channelName,chattingUsers,chattingLogs 찾아서 socket.emit('channeljoin',데이터)
+  socket.on('channelJoin', async (data) => {
+    const { username, channelName } = data;
+    const client = new MongoClient(uri);
+    try {
+      await client.connect();
+      const database = client.db('moon-discord');
+      const channelCollection = database.collection('Channel');
+      await channelCollection.updateOne(
+        { channelName: channelName }, // 채널 이름이 일치하는 문서 찾기
+        { $push: { channelUsers: username } } // channelUsers 배열에 username 추가
+      );
+      const userCollection = database.collection('User');
+      await userCollection.updateOne(
+        { username: username },
+        { $push: { channels: channelName } }
+      )
+
+      const foundData = await channelCollection.findOne({ channelName: channelName });
+      socket.emit('channelJoin', foundData);
+    } finally {
+      client.close();
     }
-    // socket.emit('channel',방금 추가한 채널)
-
-    // socket.on('channelJoin');
-    // 채널 입장
-    // parameter = {username:string, channelName:string}
-    // await db에서 채널이름 찾아서 channelUsers 배열에 유저 추가
-    // await db에서 유저이름 찾아서 userChannel 배열에 채널 추가
-    // await db에서 채널정보 channelName,chattingUsers,chattingLogs 찾아서 socket.emit('channeljoin',데이터)
-    socket.on('channelJoin', async (data) => {
-      const { username, channelName } = data;
-      const client = new MongoClient(uri);
-      try {
-        await client.connect();
-        const database = client.db('moon-discord');
-        const channelCollection = database.collection('Channel');
-        await channelCollection.updateOne(
-          { channelName: channelName }, // 채널 이름이 일치하는 문서 찾기
-          { $push: { channelUsers: username } } // channelUsers 배열에 username 추가
-        );
-        const userCollection = database.collection('User');
-        await userCollection.updateOne(
-          { username:username},
-          { $push: { channels:channelName}}
-        )
-
-        const foundData = await channelCollection.findOne({channelName:channelName});
-        socket.emit('channelJoin',foundData);
-      } finally {
-        client.close();
-      }
-    });
-
-    
-    socket.on('friend', async (data)=>{
-      try {
-        const client = new MongoClient(uri);
-        const database = client.db('moon-discord');
-        const collection = database.collection('User'); // 사용할 컬렉션 이름
-        const serverData = await collection.findOne({
-          username: data.username,
-          freinds: { $elemMatch: {$eq: data.friendName}}
-        })
-        /*친구 있는 경우 */
-        if(serverData){
-          socket.emit('friendComplete',"friend already exists");
-        }
-        /*친구 없는 경우 */
-        else{
-          await collection.updateOne(    
-              { username:data.username },
-              { $push: { friends: data.friendName }})
-              socket.emit('friendComplete',serverData);
-            }
-          }
-      finally{
-        client.close();
-      }
-    });
-    /**
-    @description 
-    // 친구 생성 
-    // parameter = {username:string, friendName:string}
-    // db에서 username 찾아서 friends배열에 friendName 추가
-    // collection name = 'User'
-     */
-    
-    // socket.emit('friend',방금 추가한 친구)
-
-    // socket.on('message');
-    // 메세지 전달
-    // parameter = {username:string, userMessage:string, channel:string}
-    // db에서 채널 이름 찾아서 chattingLogs 배열에 메시지와 메세지 생성시간 추가
-    // ex) {username:string, userMessage:string, createdAt:Timedata }
-    // collection name = 'Channel'
-
-    // socket.emit('message',방금 받은 채팅 기록) 
-    // *브로드캐스팅.. 채널에 속한 사람들에게 전달해야함. 
-
-
-    // flow 유저생성 -> 채널생성 -> 채널입장 -> 메세지 전달
-
   });
+
+
+  socket.on('friend', async (data) => {
+    try {
+      const client = new MongoClient(uri);
+      const database = client.db('moon-discord');
+      const collection = database.collection('User'); // 사용할 컬렉션 이름
+      const serverData = await collection.findOne({
+        username: data.username,
+        freinds: { $elemMatch: { $eq: data.friendName } }
+      })
+      /*친구 있는 경우 */
+      if (serverData) {
+        socket.emit('friendComplete', "friend already exists");
+      }
+      /*친구 없는 경우 */
+      else {
+        await collection.updateOne(
+          { username: data.username },
+          { $push: { friends: data.friendName } })
+        socket.emit('friendComplete', serverData);
+      }
+    }
+    finally {
+      client.close();
+    }
+  });
+  /**
+  @description 
+  // 친구 생성 
+  // parameter = {username:string, friendName:string}
+  // db에서 username 찾아서 friends배열에 friendName 추가
+  // collection name = 'User'
+   */
+
+  // socket.emit('friend',방금 추가한 친구)
+  socket.on('message', async (data) => {
+    const { username, userMessage, createAt, channelName } = data;
+    try {
+      const client = new MongoClient(uri);
+      const database = client.db('moon-discord');
+      const collection = database.collection('Channel'); // 사용할 컬렉션 이름
+      const data = await collection.findOne({ channelName: channelName })
+      const chattingData = {
+        username: username,
+        userMessage: userMessage, // 유저 이름들 배열
+        createAt: createAt
+      }
+      /*채널 있는 경우 */
+      if (data) {
+        await collection.updateOne(
+          { channelName: channelName },
+          { $push: { chattingLogs: chattingData } })
+        socket.emit('message', chattingData);
+      } else {
+        socket.emit('message', "channel is not exist");
+      }
+    }
+    finally {
+      client.close();
+    }
+  });
+  // socket.on('message');
+  // 메세지 전달
+  // parameter = {username:string, userMessage:string, channel:string}
+  // db에서 채널 이름 찾아서 chattingLogs 배열에 메시지와 메세지 생성시간 추가
+  // ex) {username:string, userMessage:string, createdAt:Timedata }
+  // collection name = 'Channel'
+
+  // socket.emit('message',방금 받은 채팅 기록) 
+  // *브로드캐스팅.. 채널에 속한 사람들에게 전달해야함. 
+
+
+  // flow 유저생성 -> 채널생성 -> 채널입장 -> 메세지 전달
+
+});
 
