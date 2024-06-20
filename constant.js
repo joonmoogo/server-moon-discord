31d641bfe10530786876acc4370fa112802c368a
joonmoogo <junemuk10@gmail.com>
2024-06-20 23:22:47 +0900

    refactor

--- constant.js
+++ constant.js
@@ -1,7 +1,9 @@
-const CLIENT_URL = 'http://localhost:3000'
+const SERVER_PORT = 3001
+const CLIENT_URL = 'http://localhost:5173'
 const MONGO_URI = 'mongodb+srv://junemuk:1998born@cluster0.deeugr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 module.exports = {
     CLIENT_URL,
-    MONGO_URI
+    MONGO_URI,
+    SERVER_PORT
 }
--- index.js
+++ index.js
@@ -7,22 +7,22 @@ require("dotenv").config();
 app.use(cors());
 const { mongoRun } = require("./Util/mongodb");
 const { socketInitializer } = require("./Controller/socket");
+const { CLIENT_URL, SERVER_PORT } = require("./constant");
 
 const server = http.createServer(app);
-server.listen(3001, async () => {
+
+server.listen(SERVER_PORT, async () => {
   await mongoRun();
   console.log("SERVER IS RUNNING ON PORT 3001");
 });
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
+
 io.on("connection", (socket) => { socketInitializer(io, socket) })
 
-app.get('/', (req, res) => {
-  res.send('<h1>hello express</h1>')
-})
 
