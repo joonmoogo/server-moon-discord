a1c3f0d3cfb1e68fae88f46a72e412a327c35f7b
joonmoogo <junemuk10@gmail.com>
2024-06-20 23:35:33 +0900

    v1 production

--- constant.js
+++ constant.js
@@ -1,9 +1,14 @@
+// const MODE = "DEV_MODE"  
+
 const SERVER_PORT = 3001
-const CLIENT_URL = 'http://localhost:5173'
+const CLIENT_URL_DEV = "http://localhost:3001"
+const CLIENT_URL_PRODUCTION = "";
 const MONGO_URI = 'mongodb+srv://junemuk:1998born@cluster0.deeugr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 module.exports = {
-    CLIENT_URL,
+    CLIENT_URL_DEV,
     MONGO_URI,
     SERVER_PORT
 }
+
+
--- index.js
+++ index.js
@@ -7,10 +7,12 @@ require("dotenv").config();
 app.use(cors());
 const { mongoRun } = require("./Util/mongodb");
 const { socketInitializer } = require("./Controller/socket");
-const { CLIENT_URL, SERVER_PORT } = require("./constant");
+const { CLIENT_URL_DEV, SERVER_PORT } = require("./constant");
+const path = require("path");
+
+app.use(express.static( path.join(__dirname, '/dist')))
 
 const server = http.createServer(app);
-
 server.listen(SERVER_PORT, async () => {
   await mongoRun();
   console.log("SERVER IS RUNNING ON PORT 3001");
@@ -18,11 +20,13 @@ server.listen(SERVER_PORT, async () => {
 
 const io = new Server(server, {
   cors: {
-    origin: CLIENT_URL,
+    origin: CLIENT_URL_DEV,
     methods: ["GET", "POST"],
   },
 });
-
 io.on("connection", (socket) => { socketInitializer(io, socket) })
 
+app.get('/', (request, response) => {
+  response.sendFile(__dirnamem, '/index.html')
+})
 
